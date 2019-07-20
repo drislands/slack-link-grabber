194bad694c3523ba9b44b794c21555d9a1afcd4a
Jeremy <jeremy.bird.237@gmail.com>
2019-07-20 12:35:35 -0400

    First fully functioning build.

--- README.md
+++ README.md
@@ -3,4 +3,12 @@ Provides a method to use a slash command in Slack and grab a meaningful link.
 
 ******
 
-Original intent is to map Jira issue keys to their respective links.
+Requires python 3 and the connexion and dotenv modules to be installed. Set a
+file in the root directory called `.env` and put two variables in there in the 
+following format:
+
+    VERIFICATION_TOKEN=insertTokenHere
+    BASE_URL=https://yourURLhere.com/
+
+The token is what your Slack integration provides for use, and the URL is what
+you want tacked on the front of the text being sent out.
--- link.py
+++ link.py
@@ -1,12 +1,22 @@
-BASE_URL = "https://google.com/"
+import os
+import dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+dotenv.load_dotenv(dotenv_path)
+verification_token = os.environ['VERIFICATION_TOKEN']
+base_url = os.environ['BASE_URL']
 
 def get():
-    return {"base URL": BASE_URL}
+    return {"base URL": base_url}
 
-def replace(text):
-    response = {
-        "response_type" : "in_channel",
-        "text" : BASE_URL + (text.decode("utf-8"))
-        # TODO: figure out why this is coming in empty!
-    }
+def replace(text,token):
+    if token == verification_token:
+        response = {
+            "response_type" : "in_channel",
+            "text" : base_url + text
+        }
+    else:
+        abort(
+            401, "Unverified request."
+        )
     return response
